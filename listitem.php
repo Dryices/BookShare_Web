f4b3c4aecde6dc035469e6f269c35cc7e70ed2b3
tanyi <tanyi@LAPTOP-828GOTL7>
2022-02-16 14:08:34 +0800

    modal is bigger

--- listitem.php
+++ listitem.php
@@ -80,27 +80,6 @@
           <br>
         </form>
          
-          <div id="paypal-button-container-P-7YF52428BY641674SMIEMUWI"></div>
-          <script src="https://www.paypal.com/sdk/js?client-id=AebQTHYqSrLkzSBiQeRGNDnx5jxbhSpRGU5-4Ekvi_QLRErQZhD9hezg_MZdMYOoyBJOgo_lpT-wJLt1&vault=true&intent=subscription" data-sdk-integration-source="button-factory"></script>
-          <script>
-            paypal.Buttons({
-                style: {
-                    shape: 'pill',
-                    color: 'gold',
-                    layout: 'horizontal',
-                    label: 'subscribe'
-                },
-                createSubscription: function(data, actions) {
-                  return actions.subscription.create({
-                    /* Creates the subscription */
-                    plan_id: 'P-7YF52428BY641674SMIEMUWI'
-                  });
-                },
-                onApprove: function(data, actions) {
-                  alert(data.subscriptionID); // You can add optional success message for the subscriber here
-                }
-            }).render('#paypal-button-container-P-7YF52428BY641674SMIEMUWI'); // Renders the PayPal button
-          </script>
          <br>
     </div> 
     
